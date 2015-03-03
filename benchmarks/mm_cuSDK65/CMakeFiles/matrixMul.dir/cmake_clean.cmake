4e4325265ac6b4001878d774825bbeaefc220292
Leiming Yu <leimingyu830@gmail.com>
2015-03-03 17:00:26 -0500

    add mm to benchmarks

---
+++ benchmarks/mm_cuSDK65/CMakeFiles/matrixMul.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/matrixMul.dir/./matrixMul_generated_matrixMul.cu.o"
+  "matrixMul.pdb"
+  "matrixMul"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/matrixMul.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
